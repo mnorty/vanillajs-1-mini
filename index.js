6b143ed78438032a313a16b8e76fd3111053380f
mnorty <mnorty@gmail.com>
2019-04-22 12:14:16 -0600

    themes working

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ function reset(){
     count = 0
     element.innerHTML = `<mark>${count}</mark>`
 }
+
+function selectTheme(theme){
+    document.getElementsByTagName('body')[0].className = theme
+    document.getElementsByTagName('main')[0].className = theme
+    const  buttons = document.getElementsByTagName("button")
+    for(let i = 0; i < buttons.length; i++)
+        buttons[i].className = theme
+}

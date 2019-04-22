b198e419c03d13ae871f9e59bd2420c05f650dbe
mnorty <mnorty@gmail.com>
2019-04-22 11:46:29 -0600

    making sense

---
+++ index.js
@@ -0,0 +1,14 @@
+let count = 0
+
+function increase(){
+    count++
+    console.log(count)
+}
+function decrease(){
+    count--
+    console.log(count)
+}
+function reset(){
+    count = 0
+    console.log(count)
+}
